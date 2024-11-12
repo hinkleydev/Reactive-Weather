fa6e3c36deae4e06f2c9ff2c4151aa704ff39071
hinkleydev <azz@hinkley.dev>
2024-11-12 11:26:39 +0000

    Quick HTML fix up

--- components/WeatherCard.js
+++ components/WeatherCard.js
@@ -17,7 +17,7 @@ function WeatherCard({ city }) {
         <div className = "img-container">
             <img className="card-img-top" src = {icons[city.forecast]} alt="Card image cap" id = "icon"/>
         </div>
-        <div class="card-body">
+        <div className="card-body">
             <h3 className="card-title">{city.city}</h3>
             <h5 className="card-text">{city.temperature}</h5>
             <h5 className="card-text">{city.forecast}</h5>
